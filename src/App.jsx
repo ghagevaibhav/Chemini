978e7ec917d0d504f46233e10fe76b1e64065c21
ghagevaibhav <ghagevaibhav@gmail.com>
2024-10-26 14:07:15 +0530

    initial ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
-  const [count, setCount] = useState(0)
-  return 
+
+return <>
+    <Sidebar />
+  </>
 }
 
 export default App
