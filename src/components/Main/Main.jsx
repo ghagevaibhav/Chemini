2c3fa5f0b10b20bdbdf80e4dd43d56a0e887cedb
ghagevaibhav <ghagevaibhav@gmail.com>
2024-10-26 15:06:35 +0530

    ui copleted

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
 import './Main.css'
+import { assets } from '../../assets/assets'
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className='main'>
+        <div className="nav">
+            <p>Chemini</p>
+            <img src={assets.user_icon} alt="" />
+        </div>
+
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello, Vaibhav</span></p>
+                <p>How can I help you today?</p>
+            </div>
+
+            <div className="cards">
+                <div className="card">
+                    <p>Suggest beautiful placs to see on an upcoming road trip</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+
+                <div className="card">
+                    <p>Briefly Summarize this concept: Urban Planning</p>
+                    <img src={assets.bulb_icon} alt="" />
+                </div>
+
+                <div className="card">
+                    <p>Brainstorm team bonding avtivities for our work retreat</p>
+                    <img src={assets.message_icon} alt="" />
+                </div>
+
+                <div className="card">
+                    <p>Give some insights on the inflation of Japanese yen</p>
+                    <img src={assets.code_icon} alt="" />
+                </div>
+            </div>
+
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" name="" id="" placeholder='Enter a prompt here' />
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+
+                <p className="bottom-info">
+                    Chemini may display inaccurate info, including about people, so double-check it's responses.
+                </p>
+            </div>
+        </div>
+    </div>
   )
 }
 
